d445745c8b53fca5b3863cd3226ae202e6be1670
karanyadavevdt25a14022-sketch <karanyadav_evdt_25a14022@dtu.ac.in>
2026-06-15 09:27:15 +0000

    basic maths done

--- 02_Basic_maths.cpp
+++ 02_Basic_maths.cpp
@@ -20,37 +20,55 @@ while(n>0){
 // factors of a number 
 
 for(int i=1;i<=n;i++){
-if(n%i==0){
+if(n%i==0 ){
     cout<<i<<" "<<n/i<<endl;
 }
 
 }
 
 
-//check prime number:: 
+//check prime number: 
 
 for(int i=1;i<=n;i++){
 
-    if(n%i==0){
-        count++
+    if(n%i==0 ){
+        count++;
+        if((n/i)!=i){
+            count++
+        }
+    }
+    if(count == 2){
+        cout<<"prime "<<endl;
+    }
+    else{
+        cout<<"not prime "<<endl;
     }
 }
 
 
+//GCD or HCF
+//brute
 
+int gcd=1,n1,n2;
+cin>>n1>>n2;
+for (int i = 1; i <=min(n1,n2); i++)
+{
+    if(n1%i==0 && n2%i==0 ){
+        gcd=i;
+    }
+}
 
+cout<<gcd;
 
+//optimized code 
 
-
-
-
-
-
-
-
-
-
-
-
+int gcd1=1;
+for (int  i = min(n1,n2); i>=1; i--)
+{
+    if(n1%i==0 && n2%i==0){
+        gcd=i;
+    }
+}
+cout<<gcd<<" ";
 
 }
