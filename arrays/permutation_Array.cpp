c00181e5e5416e8191e23ddfb85114e76beca788
karanyadavevdt25a14022-sketch <karanyadav_evdt_25a14022@dtu.ac.in>
2026-06-21 09:39:50 +0000

    done nexxt permutation

---
+++ arrays/permutation_Array.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+//     class Solution { Time complexity is O(3N) at worse case 
+// public:
+//     void nextPermutation(vector<int>& arr) {
+//        int n=arr.size(); 
+//        int index=-1;
+//        //finding the break point ;
+//        for(int i=n-2 ; i >=0 ; i--  ){
+//        if(arr[i] < arr[i+1]){
+//         index = i ; 
+//         break;
+//        }
+//        }
+//        if(index==-1){
+//             reverse(arr.begin(),arr.end());
+//             return ;
+//        }
+//        // replacing the next largest arr[index]      
+//         for(int i = n-1 ; i >= 0 ; i-- ){           
+//              if(arr[i]>arr[index])
+//              {
+//                 swap(arr[i],arr[index]);
+//                  break;
+//              }
+//         }       
+//         // sort or reverse 
+//         reverse(arr.begin() + index +1 ,arr.end());      
+//     }
+// };
+}
