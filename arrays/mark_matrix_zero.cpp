6ca59e33e42bd2d85aca0db2e94943becb4f730a
karanyadavevdt25a14022-sketch <karanyadav_evdt_25a14022@dtu.ac.in>
2026-06-23 06:58:19 +0000

    done

--- arrays/mark_matrix_zero.cpp
+++ arrays/mark_matrix_zero.cpp
@@ -68,3 +68,64 @@ using namespace std;
 //         }
 //     }
 // };
+
+// optimal solution tc= 2n^2 and spcece complexity is o(1);
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+      int r  =  matrix.size();
+      int c = matrix[0].size();
+      int col0 = 1;
+        // setting markers to 0 
+        for(int i = 0 ; i  < r ; i++){
+            if(matrix[i][0]==0) col0=0;
+            for(int j = 1 ; j < c ; j++){
+               if(matrix[i][j]==0){
+                matrix[i][0]=matrix[0][j]=0;
+               }
+            }
+
+        } 
+        //reverse traversal from last index to j-1 index ;
+        for(int i = r-1 ; i>=0;i--){
+            for(int j= c-1 ; j>=1 ; j--){
+                if(matrix[i][0] == 0|| matrix[0][j] == 0 ){
+                    matrix[i][j]=0;
+                }  
+            }
+            if(col0==0)
+                {matrix[i][0]=0;}
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
