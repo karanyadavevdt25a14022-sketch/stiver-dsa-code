0b4fadfdb6a7d6f4dba01d4fc86edc2f0489b386
karanyadav0000444-stack <karanyadav0000444@gmail.com>
2026-07-13 16:46:52 +0530

    done

--- binary_search/09_minimum_in_rotated_sorted.cpp
+++ binary_search/09_minimum_in_rotated_sorted.cpp
@@ -1,3 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    int findMin(vector<int>& arr) {
+         int n = arr.size();
+
+         int low = 0 , high = n-1,ans= INT_MAX;
+            
+          while(low<=high ){
+            int  mid = (low + high )/2;
+            if(arr[mid] >= arr[low]){
+                ans = min(ans,arr[low]);
+                low = mid + 1 ;
+            }
+            else{
+                ans =  min(ans,arr[mid]);
+                high = mid - 1 ;
+            }
+
+            
+          }
+            
+         
+         return ans ;
+    }
+};
